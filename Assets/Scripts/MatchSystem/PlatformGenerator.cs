78a73107f8c45f665b6b6ad7f6c0acd2bc2fb8bb
Stepan Lemtyugin <stepanlemtugin@gmail.com>
2025-03-26 15:53:20 +0500

    Upgare platform generator

--- Assets/Scripts/MatchSystem/PlatformGenerator.cs
+++ Assets/Scripts/MatchSystem/PlatformGenerator.cs
@@ -1,41 +1,64 @@
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlatformGenerator : MonoBehaviour
 {
     public Transform Camera;
     public GameObject PlatformPrefab;
-    public Transform FirstPlatformTransform;
-    public float platfromLength = 9.681248f;
+    public Platform FirstPlatform;
+
+    private Vector3 _rightTopOfLastPlatform;
+    public float CameraOffsetToSpawnPlatform;
+
     public int platformsAhead = 3; // Number of platforms to keep ahead
 
-    private float nextXCameraPositionToSpawnPlatform;
-    private float nextXPlatformPosition;
+    public Gradient ColorGradient;
 
     private void Start()
     {
-        nextXCameraPositionToSpawnPlatform = Camera.position.x;
-        nextXPlatformPosition = FirstPlatformTransform.position.x + platfromLength;
+        _rightTopOfLastPlatform = FirstPlatform.RightTopPoint.position;
 
         for (int i = 0; i < platformsAhead + 1; i++)
         {
-            SpawnOneMorePlatform();
+            SpawnOneMoreRandomPlatform();
         }
     }
 
     void Update()
     {
-        if (Camera.position.x > nextXCameraPositionToSpawnPlatform)
+        if (Camera.position.x > _rightTopOfLastPlatform.x + CameraOffsetToSpawnPlatform)
         {
-            SpawnOneMorePlatform();
-            nextXCameraPositionToSpawnPlatform += platfromLength;
+            SpawnOneMoreRandomPlatform();
         }
     }
 
-    public void SpawnOneMorePlatform()
+    public float MaxLength;
+    public float MinLength;
+
+    public float MaxYGlobal;
+    public float MinYGlobal;
+    public float MaxYFromPrev;
+    public float MinYFromPrev;
+
+    public void SpawnOneMoreRandomPlatform()
     {
-        GameObject newPlat = Instantiate(PlatformPrefab);
-        newPlat.transform.position = new Vector3(nextXPlatformPosition, FirstPlatformTransform.position.y, FirstPlatformTransform.position.z);
-        nextXPlatformPosition += platfromLength;
+        GameObject platrformGameObject = Instantiate(PlatformPrefab);
+        platrformGameObject.TryGetComponent<Platform>(out var platrform);
+
+        var prevY = _rightTopOfLastPlatform.y;
+        float minY = System.Math.Max(MinYGlobal, prevY + MinYFromPrev);
+        float maxY = System.Math.Min(MaxYGlobal, prevY + MaxYFromPrev);
+        float resultY = Random.Range(minY, maxY);
+        var nextLeftTopPosition = new Vector3(_rightTopOfLastPlatform.x, _rightTopOfLastPlatform.y + resultY, _rightTopOfLastPlatform.z);
+
+        var length = Random.Range(MaxLength, MinLength);
+        var height = 10000f;
+
+        var positionOnGradient = Random.value;
+        // Получаем цвет из градиента
+        Color gradientColor = ColorGradient.Evaluate(positionOnGradient);
+
+        platrform.SetParams(nextLeftTopPosition, length, height, gradientColor);
+
+        _rightTopOfLastPlatform = platrform.RightTopPoint.position;
     }
 }
